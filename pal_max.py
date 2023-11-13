49f24ad4336c5ab90bf3733716428e203c2176f8
Maksim <semimaks@mail.ru>
2023-11-13 14:02:50 +0300

    Update pal_max.py

--- pal_max.py
+++ pal_max.py
@@ -1,25 +1,35 @@
+import sys
+
 a1 = input("Ваше число:\n")
 
 
-def palindrom(a1):
-    a1 = int(a1)
-    a3 = a1
-    a2 = 0
-    while a1 > 0:
-        digit = a1 % 10
-        a1 = a1 // 10
-        a2 = a2 * 10 + digit
+def by():
+    print("Работа программы завершена!")
+    sys.exit()
 
-    print(a3 + a2)
-    x = a3 + a2
-    x = str(x)
-    if x == x[::-1]:
-        print('Палиндром!\n')
-        a1 = input("Ваше число:\n")
+
+def palindrom(a1):
+    if a1 == 'q':
+        by()
     else:
-        print('Не палиндром!\n')
-        a1 = input("Ваше число:\n")
-    palindrom(a1)
+        a1 = int(a1)
+        a3 = a1
+        a2 = 0
+        while a1 > 0:
+            digit = a1 % 10
+            a1 = a1 // 10
+            a2 = a2 * 10 + digit
+
+        print(a3 + a2)
+        x = a3 + a2
+        x = str(x)
+        if x == x[::-1]:
+            print('Палиндром!\n')
+            a1 = input("Ваше число:\n")
+        else:
+            print('Не палиндром!\n')
+            a1 = input("Ваше число:\n")
+        palindrom(a1)
 
 
 while a1 != 'q':
